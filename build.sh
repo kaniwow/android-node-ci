40c3d4f57d1cba8319c096c3bee5a7a0998c2793
kaniwow <kele20080820@hotmail.com>
2023-03-05 05:35:02 +0800

    try it

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ download_and_extract() {
 
   curl -L https://github.com/nodejs/node/archive/refs/tags/${FILENAME} > $FILENAME
   tar zxvf "$FILENAME"
-  cp android-configure node-$TAG/
+  # cp android-configure node-$TAG/
 }
 
 
@@ -32,7 +32,10 @@ build-android() {
     # sed -i "s|// Setup for shared library export.|#undef V8_TRAP_HANDLER_VIA_SIMULATOR\n#undef V8_TRAP_HANDLER_SUPPORTED\n#define V8_TRAP_HANDLER_SUPPORTED false\n\n// Setup for shared library export.|" deps/v8/src/trap-handler/trap-handler.h
   fi
 
-  ./android-configure $ANDROID_NDK_HOME $ANDROID_ABI 29 $TAG
+  cd  node-$TAG/
+  ./android-configure $ANDROID_NDK_HOME 29 $ANDROID_ABI
+  # ./android-configure $ANDROID_NDK_HOME $ANDROID_ABI 29 $TAG
+
 
   make -j4
 }
